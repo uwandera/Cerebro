1d5eb824fec0137699df4b1075c97f70119c6240
uwandera <tomuwande@gmail.com>
2025-09-17 12:16:23 -0300

    tava com erros de parametro main, nao tinha percebido!

--- src/edu/thomas/primeirasemana/OperadoresUnarios.java
+++ src/edu/thomas/primeirasemana/OperadoresUnarios.java
@@ -2,12 +2,12 @@ package edu.thomas.primeirasemana;
 
 public class OperadoresUnarios {
     
-    public static void main(){
+    public static void main(String [] args){
         int numero = 5;
 
-            numero = -numero;
+            
         
-            System.out.println(numero);
+            System.out.println(-numero);
 
     }
 }
